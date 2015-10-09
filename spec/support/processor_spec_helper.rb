4664149d60f6dd5fecdea82db8dc54fefefcc105
Zequez <zequez@gmail.com>
2015-10-09 02:00:09 -0300

    Fix everything left from the Loader refactor

--- spec/support/processor_spec_helper.rb
+++ spec/support/processor_spec_helper.rb
@@ -2,6 +2,8 @@ module ProcessorSpecHelper
   def scrap(page_url, headers = {}, &add_to_queue)
     response = Typhoeus.get(page_url, headers: headers)
     add_to_queue ||= lambda{|url|}
-    processor_class.new(response, &add_to_queue).process_page
+    scrap_request = Scrapers::RootScrapRequest.new(page_url, nil, nil, lambda{ |all_data, data| })
+    scrap_request.set_response response
+    processor_class.new(scrap_request, &add_to_queue).process_page
   end
 end
