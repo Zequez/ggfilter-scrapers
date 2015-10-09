e169ae7d20caf9e26f8bded56c740c63c924f633
Zequez <zequez@gmail.com>
2015-10-09 03:09:29 -0300

    Added default table to migrations

--- lib/scrapers/base_migration.rb
+++ lib/scrapers/base_migration.rb
@@ -1,6 +1,6 @@
 module Scrapers
   class BaseMigration < ActiveRecord::Migration
-    def initialize(*args, table_name: nil)
+    def initialize(*args, table_name: :games)
       @table_name = table_name
       super(*args)
     end
